f8c43b936cfc47ca5dd83c722f329cf372f60a54
Jon Taylor <jont@cakesolutions.net>
2017-06-08 11:36:00 +0100

    Now using jsc elements rather than oneof

--- client/tests/autoSolveTests.js
+++ client/tests/autoSolveTests.js
@@ -1,25 +1,9 @@
 import 'babel-polyfill';
 import jsc from 'jsverify';
 import { initialAutoSolveSet, generateGuess, evaluateGuess } from '../js/logic';
-import { Peg } from '../js/constants';
+import { Peg as P } from '../js/constants';
 
-const stringsToPegs = strings => strings.map(stringToPeg);
-
-const stringToPeg = string => {
-    switch (string) {
-        case 'R': return Peg.RED;
-        case 'G': return Peg.GREEN;
-        case 'B': return Peg.BLUE;
-        case 'Y': return Peg.YELLOW;
-        case 'BL': return Peg.BLACK;
-        case 'WH': return Peg.WHITE;
-        default: throw new Error(`Unknown peg colour, "${string}".`);
-    }
-};
-
-const autoSolve = strings => {
-
-    const secret = stringsToPegs(strings);
+const autoSolve = secret => {
 
     const loop = state => {
         const result = generateGuess(state.autoSolveSet, state.autoSolveUsed, state.lastGuess, state.lastGuessFeedback);
@@ -55,13 +39,7 @@ describe('autoSolve', () => {
     };
 
     it('finds the correct solution within 5 attempts', () => {
-        const arbPeg = jsc.oneof([
-            jsc.constant('R'),
-            jsc.constant('G'),
-            jsc.constant('B'),
-            jsc.constant('Y'),
-            jsc.constant('BL'),
-            jsc.constant('WH')]);
+        const arbPeg = jsc.elements([P.RED, P.GREEN, P.BLUE, P.YELLOW, P.BLACK, P.WHITE]);
         const arbSecret = jsc.tuple([arbPeg, arbPeg, arbPeg, arbPeg]);
         const prop = jsc.forall(arbSecret, autoSolve);
         jsc.check(prop, opts);
